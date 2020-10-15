0c318dd31d9fc1f07754a8c31b3d27f85e39dffe
Kylevb405 <kylevanburen405@gmail.com>
2020-10-14 23:45:06 -0400

    finished

--- index.js
+++ index.js
@@ -125,7 +125,7 @@ const carObj = new Car({
   milesPerGallon: 13.84,
 }) 
 
-// carObj
+carObj.fill(10);
 
 /*
   TASK 3
@@ -134,9 +134,23 @@ const carObj = new Car({
     - Besides the methods on Person.prototype, babies have the ability to `.play()`:
         + Should return a string "Playing with x", x being the favorite toy.
 */
-function Baby() {
 
+function Baby(name, age, favoriteToy) {
+
+  Person.call(this, name, age)
+
+  this.favoriteToy = favoriteToy;
+
+  Baby.prototype.play = function () {
+    return `Playing with ${this.favoriteToy}`
+  }
 }
+
+Baby.prototype = Object.create(Person.prototype);
+
+const babyObj = new Baby ('Kyle Jr.', 1, 'Race Car')
+
+babyObj.play()
 
 /* 
   TASK 4
